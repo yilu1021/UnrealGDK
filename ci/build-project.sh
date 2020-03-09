a5c017187bf5f94c7be2f1fe96f58f22967d3a7b
Jessica Falk <jessica@improbable.io>
2020-03-09 13:28:40 +0000

    more fixing up of paths

--- ci/build-project.sh
+++ ci/build-project.sh
@@ -20,20 +20,20 @@ fi
 
 # The Plugin does not get recognised as an Engine plugin, because we are using a pre-built version of the engine
 # copying the plugin into the project's folder bypasses the issue
-ln -s ${TESTP_REPO_PATH}/Game/Plugins ${GDK_HOME}
+ln -s ${GDK_HOME} ${TESTP_REPO_PATH}/Game/Plugins 
 
 # Disable tutorials, otherwise the closing of the window will crash the editor due to some graphic context reason
 echo "`r`n[/Script/IntroTutorials.TutorialStateSettings]`r`nTutorialsProgress=(Tutorial=/Engine/Tutorial/Basics/LevelEditorAttract.LevelEditorAttract_C,CurrentStage=0,bUserDismissed=True)`r`n" >> ${UNREAL_PATH}/Engine/Config/BaseEditorSettings.ini
 
-echo "Generating project files"
-${ENGINE_DIRECTORY}/Engine/Build/BatchFiles/Mac/Build.sh -projectfiles -project="${TEST_REPO_UPORJECT_PATH}" -game -engine -progress
+echo "--- Generating project files"
+${UNREAL_PATH}/Engine/Build/BatchFiles/Mac/Build.sh -projectfiles -project="${TEST_REPO_UPORJECT_PATH}" -game -engine -progress
 if [[ $? -ne 0 ]]; then
   echo "Failed to generate files for the testing project."
   exit 1
 fi
 
-echo "Building project"
-${ENGINE_DIRECTORY}/Engine/Build/BatchFiles/Mac/XcodeBuild.sh ${BUILD_TARGET} ${BUILD_PLATFORM} ${BUILD_STATE} "${TEST_REPO_UPORJECT_PATH}" -buildscw
+echo "--- Building project"
+${UNREAL_PATH}/Engine/Build/BatchFiles/Mac/Build.sh ${BUILD_TARGET} ${BUILD_PLATFORM} ${BUILD_STATE} "${TEST_REPO_UPORJECT_PATH}"
 if [[ $? -ne 0 ]]; then
   echo "Failed to build testing project."
   exit 1
