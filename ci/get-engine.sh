a6d3147b23a3998c1a9a0c035396001824be9681
jessicafalk <31853332+jessicafalk@users.noreply.github.com>
2020-03-10 06:52:48 +0000

    remove some of the hardcoding

--- ci/get-engine.sh
+++ ci/get-engine.sh
@@ -25,20 +25,19 @@ pushd "${GDK_HOME}"
         # If so, retrieve the version of the latest build from GCS, and use that going forward.
         HEAD_VERSION_PREFIX="HEAD "
         if [[ ${VERSION_DESCRIPTION} = ${HEAD_VERSION_PREFIX}* ]]; then
+        # TODO override this for now
             VERSION_BRANCH=${VERSION_DESCRIPTION#"${HEAD_VERSION_PREFIX}"} # Remove the prefix to just get the branch name
             VERSION_BRANCH= ${VERSION_BRANCH///_} # Replace / with _ since / is treated as the folder seperator in GCS
-
+VERSION_BRANCH="feature_macos-buildkite"
             # Download the head pointer file for the given branch, which contains the latest built version of the engine from that branch
-            HEAD_POINTER_GCS_PATH="gs://${GCS_PUBLISH_BUCKET}/HEAD/${VERSION_BRANCH}.version"
+            HEAD_POINTER_GCS_PATH="gs://${GCS_PUBLISH_BUCKET}/HEAD/mac-${VERSION_BRANCH}.version"
             UNREAL_VERSION=$(gsutil cp ${HEAD_POINTER_GCS_PATH} -) # the '-' at the end instructs gsutil to download the file and output the contents to stdout
         else 
             UNREAL_VERSION=${VERSION_DESCRIPTION}
         fi
     popd
 
-    # TODO hardocde the version for now till we publish the new change in the engine
-    UNREAL_VERSION="UnrealEngine-MacOS-b8e0714e146a5ce7479baab10f6ce0c5dd899022"
-
+    echo ${UNREAL_VERSION}
     ## Create an UnrealEngine-Cache directory if it doesn't already exist.
     mkdir -p ${ENGINE_CACHE_DIRECTORY}
 
