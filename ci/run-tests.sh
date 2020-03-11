97ecd8066f571de1e33565730a71a8bb1a6c8863
Jessica Falk <jessica@improbable.io>
2020-03-11 11:51:30 +0000

    remove powershell bits

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -15,25 +15,16 @@ if [[ -n "${RUN_WITH_SPATIAL}" ]]; then
 	cp "${TEST_REPO_PATH}/spatial/snapshots/${TEST_REPO_MAP}.snapshot" "${TEST_REPO_PATH}/spatial/snapshots/default.snapshot" 
 fi
 
-
+rm -r ci/${REPORT_OUTPUT_PATH}
 mkdir ci/${REPORT_OUTPUT_PATH}
-# Create the TestResults directory if it does not exist, for storing results
-New-Item -Path "$PSScriptRoot" -Name "$report_output_path" -ItemType "directory" -ErrorAction SilentlyContinue
-$output_dir = "$PSScriptRoot\$report_output_path"
-
-# We want absolute paths since paths given to the unreal editor are interpreted as relative to the UE4Editor binary
-# Absolute paths are more reliable
-$ue_path_absolute = Force-ResolvePath $unreal_editor_path
-$uproject_path_absolute = Force-ResolvePath $uproject_path
-$output_dir_absolute = Force-ResolvePath $output_dir
 
 ${UNREAL_EDITOR_PATH} \
-	"${UPROJECT_PATH}" \
+	"$(pwd)/${UPROJECT_PATH}" \
 	"${TEST_REPO_MAP}"  \
 	-execCmds="Automation RunTests ${TESTS_PATH}; Quit" \
 	-TestExit="Automation Test Queue Empty" \
-	-ReportOutputPath="ci/${REPORT_OUTPUT_PATH}" \
-	-ABSLOG="${LOG_FILE_PATH}" \
+	-ReportOutputPath="$(pwd)/ci/${REPORT_OUTPUT_PATH}" \
+	-ABSLOG="$(pwd)/${LOG_FILE_PATH}" \
 	-nopause \
 	-nosplash \
 	-unattended \
